e5f9d999ea716808ed9a957fef0cb4e67a36e5e5
nancytao <nancy.tao42@gmail.com>
2018-11-14 13:03:28 -0500

    Fixed homepage showing user name

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,6 @@ import authClient from '../Auth';
 export default () => (
   <Typography variant="display1">
     Hello!
-    {authClient.isAuthenticated() ? " This is the homepage for " + (authClient.getUser() && authClient.getUsername()) : " Please log in."}
+    {authClient.isAuthenticated() ? " This is the homepage for " + authClient.getUser() + ", " + authClient.getUsername() : " Please log in."}
   </Typography>
 );
